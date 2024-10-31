404f342ce6337c94748e8457fee5a756cf4c1b0b
jasminelli <jasco88017@gmail.com>
2024-10-31 15:38:47 -0400

    Improve preferences flow (#9)

--- app/(drawer)/index.tsx
+++ app/(drawer)/index.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'expo-router';
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { View, Text, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   const router = useRouter();
@@ -14,26 +14,36 @@ export default function Home() {
     'To give you the best possible experience, we need to ask you a few questions about your perfect day.';
 
   return (
-    <ScrollView className="flex-1 px-10 py-10">
-      <View className="mb-10 items-center">
-        <Image
-          source={require('../../assets/logo.png')}
-          className="h-30 w-30"
-          resizeMode="contain"
-        />
-        <Text className="mb-5 text-3xl font-bold">{titleText}</Text>
-        <Text className="mb-10 text-xl">{introText}</Text>
-        <Text className="mb-5 text-2xl font-bold">{instructionHeader}</Text>
-        <Text className="mb-6 text-xl">{instructionText}</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'space-between',
+          paddingHorizontal: 40,
+          paddingVertical: 20,
+        }}>
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+          <View className="items-center">
+            <Image
+              source={require('../../assets/logo.png')}
+              className="mb-10 h-40 max-h-60 w-full"
+              resizeMode="contain"
+            />
+            <Text className="mb-4 text-center text-3xl font-bold">{titleText}</Text>
+            <Text className="mb-10 text-center text-lg">{introText}</Text>
+            <Text className="mb-2 text-center text-2xl font-bold">{instructionHeader}</Text>
+            <Text className="mb-6 text-center text-lg">{instructionText}</Text>
+          </View>
+        </ScrollView>
         <TouchableOpacity
-          className="rounded bg-accentPurple p-3"
+          className="mb-4 rounded bg-accentPurple px-4 py-2"
           onPress={() => router.push('/Login')}>
-        <Text className="text-center font-bold text-lg text-white">Get Started</Text>
+          <Text className="text-center text-lg font-bold text-white">Get Started</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => router.push('/Login')}>
-          <Text className="mt-8 text-sm text-gray-500">Already have an account? Login</Text>
+          <Text className="text-center text-sm text-gray-500">Already have an account? Login</Text>
         </TouchableOpacity>
       </View>
-    </ScrollView>
+    </SafeAreaView>
   );
 }
